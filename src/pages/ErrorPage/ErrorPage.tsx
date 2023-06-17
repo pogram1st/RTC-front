f28408227252805ac1b783aa57374f2101ca9c99
Konstantin Kolupaev <kolupaevkonstantin18@gmail.com>
2023-06-18 04:09:13 +0700

    first commit

---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,8 @@
+const ErrorPage = () => {
+    return (
+        <div className={'error-block'}>
+            <h1>Ooops!</h1>
+            <h3>Not Found!</h3>
+        </div>
+    )
+}
---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div className="container">
+      <p>Говно</p>
+    </div>
+  );
+}
+
+export default App;
